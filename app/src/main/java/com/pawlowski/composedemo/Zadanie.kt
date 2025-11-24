252f41c7ebe61afbc423e18128082a9efc7968cb
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2025-11-24 16:05:31 +0100

    Add solution to exercise

--- app/src/main/java/com/pawlowski/composedemo/Zadanie.kt
+++ app/src/main/java/com/pawlowski/composedemo/Zadanie.kt
@@ -1,8 +1,35 @@
 package com.pawlowski.composedemo
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
 
+@PreviewWithBackground
 @Composable
 fun Zadanie() {
+    val count = remember { mutableStateOf(10) }
+    Column {
+        Text(text = count.value.toString())
+
+        Button(onClick = {
+            count.value = 10
+        }) {
+            Text(text = "Restart")
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(1000L)
+            if(count.value > 0) {
+                count.value -= 1
+            }
+        }
+    }
 }
