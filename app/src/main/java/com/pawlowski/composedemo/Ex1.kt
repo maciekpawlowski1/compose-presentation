a1a15ac1f9fa1d479dc9554a4f4c1a6b80201cfa
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2025-11-24 14:45:24 +0100

    Improve

--- app/src/main/java/com/pawlowski/composedemo/Ex1.kt
+++ app/src/main/java/com/pawlowski/composedemo/Ex1.kt
@@ -2,19 +2,12 @@ package com.pawlowski.composedemo
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pawlowski.composedemo.ui.theme.ComposeDemoTheme
 
 @Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
